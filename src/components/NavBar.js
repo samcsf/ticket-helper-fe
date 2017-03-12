a25aeeae3596aa9d16b0852d2c657f4e4dd0cfb7
samcsf <samfc@me.com>
2017-03-12 16:46:06 +0800

    extract searchbar from navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import request from 'superagent';
+import SearchBar from './SearchBar';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap';
 
@@ -65,40 +66,5 @@ function getTickets(from,to){
     });
 }
 
-class SearchBar extends Component{
-    constructor(props){
-        super(props);
-        this.submit = this.submit.bind(this);
-        this.update = this.update.bind(this);
-    }
-
-    submit(e){
-        e.preventDefault();
-        this.props.getTickets.call(this,this.fromText.value,this.toText.value);
-    }
-
-    update(tickets){
-        this.props.update(tickets);
-    }
-    
-    render(){
-        return(
-            <form className="navbar-form navbar-left">
-                <div className="form-group">
-                    <input type="text" 
-                        ref={i=>{this.fromText = i}} 
-                        className="form-control" 
-                        placeholder="From" />
-                    <input type="text" 
-                        ref={i=>{this.toText = i}} 
-                        className="form-control" 
-                        placeholder="To" />
-                </div>
-            <button type="submit" className="btn btn-default" 
-                onClick={this.submit}>Go!</button>
-            </form>
-        );
-    }
-};
 
 export default NavBar;
