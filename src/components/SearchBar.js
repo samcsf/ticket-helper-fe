ec630bd38bfbef4424319dd639049bfe6a18ad22
samcsf <samfc@me.com>
2017-03-15 01:58:08 +0800

    ✨ Better Searchbar Look &   prefill tomorrow date

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -4,31 +4,37 @@ import 'bootstrap';
 
 const SearchBar = ({onSubmitClick})=>{
     let from,to,date,student;
-    let today = new Date().toISOString().substr(0,10);
+    let today = new Date();
+    let yyyy = today.getFullYear();
+    let mm = today.getMonth()+1;
+    let dd = today.getDate();
+    let tomorrow = `${yyyy}-${mm>10?mm:'0'+mm}-${dd>10?dd:'0'+dd}`;
     return(
     <form className="navbar-form navbar-left">
-        <div className="form-group">
-            <input type="text" 
-                ref={i=>{from = i}} 
-                className="form-control" 
-                placeholder="From" />
-            <input type="text" 
-                ref={i=>{to = i}} 
-                className="form-control" 
-                placeholder="To" />
-            <input type="text" 
-                ref={i=>{date = i}} 
-                className="form-control" 
-                defaultValue={today}
-                placeholder="Date YYYY-MM-DD" />
-            <input type="checkbox" 
-                ref={i=>{student = i}} 
-                className="form-control" />
+        <div className="input-group">
+            <div className="input-group-btn">
+                <input type="text" ref={i=>{from = i}} 
+                    className="form-control" placeholder="From" />
+                    
+                <input type="text" ref={i=>{to = i}} 
+                    className="form-control" placeholder="To" />
+                    
+                <input type="text" ref={i=>{date = i}} defaultValue={tomorrow}
+                    className="form-control" placeholder="Date YYYY-MM-DD" />
+
+                <span className="input-group-addon"> 
+                    Student{' '}
+                    <input type="checkbox" ref={i=>{student = i}} />
+                </span>
+                
+                <span className="input-group-btn">
+                    <button type="submit" className="btn btn-default" 
+                        onClick={(e)=>{e.preventDefault();
+                            onSubmitClick(from.value,to.value,date.value,student.checked)}}>Go!
+                    </button>
+                </span>
+            </div>
         </div>
-        <button type="submit" className="btn btn-default" 
-            onClick={(e)=>{
-                e.preventDefault();
-                onSubmitClick(from.value,to.value,date.value,student.checked)}}>Go!</button>
     </form>);
 };
 
