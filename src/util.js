ca92b0eb5bf8dc4b6610386b21b4285a904e069f
samcsf <samfc@me.com>
2017-03-16 23:43:10 +0800

    💡 New function addDay()

--- src/util.js
+++ src/util.js
@@ -1,12 +1,10 @@
 /**
- * Input: new Date(1993,0,22),'@yyyy-@mm-@dd'
- *        new Date(1993,0,22),'@yyyy/@mm/@dd'
- * Output: '1993-01-22'
- *         '1993/01/22'
- *  
- * @param {Date} date {String} format 
+ * fixLenNum(number,len)
+ * 
+ * Return fixed length number fill with zero
+ * @param {number} number 
+ * @param {number} len 
  */
-
 export const fixLenNum = (number,len)=>{
     if (isNaN(parseInt(number,10))||isNaN(parseInt(len,10)))
         throw new TypeError('Number&Len should be both numeric!');
@@ -15,6 +13,17 @@ export const fixLenNum = (number,len)=>{
     return result.slice(1);
 }
 
+/**
+ * formatDate(date,format)
+ * 
+ * Input: new Date(1993,0,22),'@yyyy-@mm-@dd'
+ *        new Date(1993,0,22),'@yyyy/@mm/@dd'
+ * Output: '1993-01-22'
+ *         '1993/01/22'
+ *  
+ * @param {Date} date 
+ * @param {String} format 
+ */
 export const formatDate = (date,format) =>{
     let yyyy = date.getFullYear();
     //Month of Date start from 0 to 11
@@ -34,77 +43,11 @@ export const formatDate = (date,format) =>{
     return result;
 }
 
-export const parser = format =>{
-    const TOKEN_LIST = ['yyyy','mm','dd'];
-    const ESCAPE = '@';
-    const icc = ignoreCaseCmp; //shorthand
+export const addDay = (date,factor)=>{
+    let sec = 1000;
+    let min = 60 * sec;
+    let hour = 60 * min;
+    let day = 24 * hour;
 
-    let charList = format.split('');
-    let captureList = [];
-    let temp = '';
-    let token = '';
-    let isEsc = false;
-    charList.map(c=>{
-        if(!isEsc&&icc(c,ESCAPE)){
-            isEsc = true;
-            return;
-        }
-
-        if(isEsc){
-            //capture the next letter 
-            token += c;
-            //check with tokens
-
-            let matchIdx = fetchItemFromBegin(token,TOKEN_LIST,icc);
-            // no match
-            if(matchIdx===undefined){
-               temp += (ESCAPE+token);
-               token = '';
-               isEsc = false;
-               //next item
-               return;    
-            }else{
-                // exact
-                if(fetchItem(token,TOKEN_LIST,icc)!==undefined){
-                    captureList.push(token);
-                    token = '';
-                    isEsc = false;
-                    return;
-                }else return;
-            }
-        }else
-            temp += c;
-    });
-
-    console.log(temp);
-    console.log(captureList);
-    
-    return captureList.length;
+    return new Date(date.getTime()+factor*day);
 }
-
-function ignoreCaseCmp(a,b){
-    return (a.toLocaleLowerCase() === b.toLocaleLowerCase());
-}
-
-//below functions need enhancement 
-function fetchItemFromBegin(keyword,array,cmpFunc){
-    let cmp = (a,b)=>a===b;
-    if (typeof cmpFunc === 'function')
-        cmp = cmpFunc;
-    for (let item of array){
-        if(cmp(keyword,item.substr(0,item.length>keyword.length?keyword.length:item.length)))
-            return array.indexOf(item);
-    }
-} 
-
-function fetchItem(keyword,array,cmpFunc){
-    let cmp = (a,b)=>a===b;
-    if (typeof cmpFunc === 'function')
-        cmp = cmpFunc;
-    for (let item of array){
-        if(cmp(keyword,item))
-            return array.indexOf(item);
-    }
-}
-
-
--- src/util.test.js
+++ src/util.test.js
@@ -1,4 +1,4 @@
-import {parser,formatDate,fixLenNum} from './util';
+import {addDay,formatDate,fixLenNum} from './util';
 
 test('Fix len return',()=>{
     expect(fixLenNum(2,2)).toBe('02');
@@ -8,9 +8,6 @@ test('Format today with -',()=>{
     expect(formatDate(new Date(),'@yyyy-@mm-@dd')).toBe('2017-03-16');
 });
 
-test('parse',()=>{
-    expect(parser('adf@yyyyder@Mmfsfgsf@dD')).toBe(3);
-});
-test('parse2',()=>{
-    expect(parser('adf@yyyder@Mmf@ksfgsf@dD')).toBe(2);
+test('Add day',()=>{
+    expect(addDay(new Date(),1).getDate()).toBe(17);
 });
